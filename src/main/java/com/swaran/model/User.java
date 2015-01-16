f5dc74069aa99290bd42d8110a44b3b66e2b9282
SwaranS <singhsw@tcd.ie>
2015-01-16 23:14:10 +0000

    Added Models

---
+++ src/main/java/com/swaran/model/User.java
@@ -0,0 +1,43 @@
+package com.swaran.model;
+
+/**
+ * Created by Home on 16/01/2015.
+ */
+public class User {
+
+    private String firstname;
+    private String lastname;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
